9bd794054b7d0e6050a78659dde94278d8b0c873
Jacob Matias <matiasdajacob@gmail.com>
2023-07-12 00:21:45 -0300

    ProductMock

---
+++ src/productsMock.js
@@ -0,0 +1,62 @@
+export const products = [
+  {
+    id: 1,
+
+    title: "Everlast",
+
+    price: 15000,
+
+    stock: 2,
+
+    description: "Esta es la mejor remera",
+
+    category: "indumentaria",
+
+    img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669326932/free-metcon-4-zapatillas-de-entrenamiento-TQMCZg_fvia3m.png",
+  },
+  {
+    id: 2,
+
+    title: "ADIDAS",
+
+    price: 15000,
+
+    stock: 2,
+
+    description: "Estas son las mejores zapatillas de boxeo",
+
+    category: "indumentaria",
+
+    img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669326932/free-metcon-4-zapatillas-de-entrenamiento-TQMCZg_fvia3m.png",
+  },
+  {
+    id: 3,
+
+    title: "PRETORIAN",
+
+    price: 15000,
+
+    stock: 2,
+
+    description: "Estas son guantes pro",
+
+    category: "guantes",
+
+    img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669326932/free-metcon-4-zapatillas-de-entrenamiento-TQMCZg_fvia3m.png",
+  },
+  {
+    id: 4,
+
+    title: "TWINS",
+
+    price: 15000,
+
+    stock: 2,
+
+    description: "Estas son los mejores guantes",
+
+    category: "guantes",
+
+    img: "https://res.cloudinary.com/dnqfh2chg/image/upload/v1669326932/free-metcon-4-zapatillas-de-entrenamiento-TQMCZg_fvia3m.png",
+  },
+];
