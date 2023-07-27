410ab9385c3947efb03e8966c957db9971913db7
Jacob Matias <matiasdajacob@gmail.com>
2023-07-27 00:26:42 -0300

    ICONO

--- src/components/layout/navBar/Navbar.jsx
+++ src/components/layout/navBar/Navbar.jsx
@@ -1,48 +1,32 @@
-
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import CardWidget from '../../common/cardWidget/CardWidget';
-import { Link } from 'react-router-dom';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import CardWidget from "../../common/cardWidget/CardWidget";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link to={"/"}>
-            Balboa Store
-            </Link>
+            <Link to={"/"}>🥊</Link>
           </Typography>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link to={"/category/guantes"}>
-            Guantes
-            </Link>
+            <Link to={"/"}>Todos</Link>
           </Typography>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <Link to={"/category/indumentaria"}>
-            Indumentaria
-            </Link>
+            <Link to={"/category/guantes"}>Guantes</Link>
           </Typography>
-          <CardWidget/>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link to={"/category/indumentaria"}>Indumentaria</Link>
+          </Typography>
+          <CardWidget />
         </Toolbar>
       </AppBar>
     </Box>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
